f4214128b3068972194bd4b9975f2ce0dc09d328
martynovaNora <87224283+martynovaNora@users.noreply.github.com>
2022-01-10 21:42:35 +0300

    Add all my files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {PureComponent} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MainMenu from './components/MainMenu';
+
+
+
+class App extends PureComponent {
+  render () {
+    return (
+   <>
+   <MainMenu />
+   </>
+ );
+}
+  }
+  
+export default App;
---
+++ src/components/MainMenu.js
@@ -0,0 +1,18 @@
+import React, {PureComponent} from 'react';
+import { Navbar } from 'react-bootstrap';
+
+
+class MainMenu extends PureComponent {
+    render() {
+ return (
+    <>
+    <Navbar>
+    
+</Navbar>
+</>
+    );
+}
+}
+   
+
+export default MainMenu;
